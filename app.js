5ae194439ab0d835d433d13182d03ab3507ca88a
Yassiz <yasaman109@Yasaman-Zabihis-MacBook-Pro.local>
2016-04-18 18:59:45 -0700

    removed comments

--- app.js
+++ app.js
@@ -9,25 +9,31 @@ $(document).ready(function(){
 		$.ajax({
 			url: "http://www.omdbapi.com?s="+title,
 			type: "GET",
-			success: function(movie) {
-				console.log(movie.Search);
-				var search_arr = movie.Search;
-				 search_arr.forEach(function (element) {
-				 	var id = element.imdbID;
-				 	$.ajax({
-						url: "http://www.omdbapi.com/?i="+id,
-						type: "GET",
-						success: function(movie) {
-							$("#search-input-box").fadeOut( "slow");
-				 			$(".movie-card").show("slide", { direction: "left" }, 1000);
-				 			$("#movie-card-container").append(movieTemplate(movie));
-				 			$("#search-again-box").show('slow');
-						},
-						error: function() {
-							alert("Error!");
-						}
+			success: function(results) {
+				console.log(results.Search);
+				if(results.Search){
+					var search_arr = results.Search;
+					search_arr.forEach(function (element) {
+						var id = element.imdbID;
+						$.ajax({
+							url: "http://www.omdbapi.com/?i="+id,
+							type: "GET",
+							success: function(movie) {
+								$("#search-input-box").fadeOut( "slow");
+					 			$(".movie-card").show("slide", { direction: "left" }, 1000);
+					 			$("#movie-card-container").append(movieTemplate(movie));
+					 			$("#search-again-box").show('slow');
+							},
+							error: function() {
+								alert("Error!");
+							}
+						});
 					});
-				 });
+				}
+				else {
+					alert('Please enter a valid title!');
+					$("#get-movie-form")[0].reset();
+				}
 			},
 			error: function() {
 				alert("Error!");
